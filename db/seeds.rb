fb9a1b7081c2276862d8d8f5838a57f9343d5761
Siobhan Coady <siobhan.coady@gmail.com>
2017-04-28 20:49:52 -0700

    Create project with User, Organization, and Technology models

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+20.times do
+  Organization.create(name: Faker::Company.name,
+                      employees: rand(1..100),
+                      manager: Faker::Name.first_name,
+                      is_published: true,
+                      city: Faker::Address.city,
+                      street: Faker::Address.street_address,
+                      province: Faker::Address.state,
+                      postal_code: Faker::Address.zip_code,
+                      overview: Faker::RickAndMorty.quote,
+                      website: Faker::Internet.url,
+                      twitter: Faker::Internet.url('twitter.com')
+  )
+  puts "Org created!"
+end
+
+20.times do
+  organization = Organization.all.sample
+  User.create(first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email,
+              password: '1234567',
+              organization_id: organization.id,
+              is_admin: false
+  )
+  puts "User created!"
+end
