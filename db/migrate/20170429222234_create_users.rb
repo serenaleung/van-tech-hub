50dece8900b50ce7f720b4da7f0bce6d4d9c3d8e
Siobhan Coady <siobhan.coady@gmail.com>
2017-04-29 15:23:19 -0700

    Create user model

---
+++ db/migrate/20170429222234_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.references :organization, foreign_key: true
+      t.boolean :is_admin, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429222046) do
+ActiveRecord::Schema.define(version: 20170429222234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,4 +31,15 @@ ActiveRecord::Schema.define(version: 20170429222046) do
     t.datetime "updated_at",                  null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "organization_id"
+    t.boolean  "is_admin",        default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.index ["organization_id"], name: "index_users_on_organization_id", using: :btree
+  end
+
+  add_foreign_key "users", "organizations"
 end
