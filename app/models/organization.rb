cb3eec1d82d002b6c1843f623413a8d5f979a98e
Serena Leung <serenaleung1@gmail.com>
2017-05-01 01:18:35 -0700

    Added News page, fixed styling for other pages

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,5 +1,5 @@
 class Organization < ApplicationRecord
-<<<<<<< HEAD
+
 has_many :users, dependent: :nullify
 has_many :adapts, dependent: :destroy
 has_many :adapted_technologies, through: :adapts, source: :technology
