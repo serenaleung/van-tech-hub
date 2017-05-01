4d0ea9c6f1e84f96a08eae97958c545a22fda40e
Chase Sang <chase.sang@gmail.com>
2017-05-01 00:40:00 -0700

    "set up for richard"

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,16 @@
 class Organization < ApplicationRecord
-<<<<<<< HEAD
+
 has_many :users, dependent: :nullify
 has_many :adapts, dependent: :destroy
 has_many :adapted_technologies, through: :adapts, source: :technology
 
+validates :name, presence: true, uniqueness: { case_sensitive: false}
+validates :employee, { presence: true, }
+validates :city, { presence: true, }
+validates :street, { presence: true, }
+validates :province, { presence: true, }
+validates :website, { presence: true, }
+validates :adapted_technologies, { presence: true, }
 
   geocoded_by :full_street_address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
