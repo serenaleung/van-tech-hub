e29e892cf2338303ad7643a040a3af2015764161
Siobhan Coady <siobhan.coady@gmail.com>
2017-04-30 23:23:23 -0700

    Add logo column to organizations table

---
+++ db/migrate/20170501052313_add_logo_column_to_organizations.rb
@@ -0,0 +1,5 @@
+class AddLogoColumnToOrganizations < ActiveRecord::Migration[5.0]
+  def change
+    add_column :organizations, :logo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430215355) do
+ActiveRecord::Schema.define(version: 20170501052313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170430215355) do
     t.datetime "updated_at",                  null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.string   "logo"
   end
 
   create_table "search_terms", force: :cascade do |t|
@@ -76,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170430215355) do
 
   create_table "technologies", force: :cascade do |t|
     t.string   "language"
+    t.string   "framework"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
